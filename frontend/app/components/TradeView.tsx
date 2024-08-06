5bf318d8166348d6966634dfb3c103b0a6d2b01c
anmolchhabra21 <anmolchhabra71500@gmail.com>
2024-08-06 20:49:42 +0530

    minor changes, most frontend components done

--- frontend/app/components/TradeView.tsx
+++ frontend/app/components/TradeView.tsx
@@ -11,39 +11,39 @@ export function TradeView({
   const chartRef = useRef<HTMLDivElement>(null);
   const chartManagerRef = useRef<ChartManager>(null);
 
-  const init = async () => {
-    let klineData: KLine[] = [];
-    try {
-      klineData = await getKlines(market, "1h", Math.floor((new Date().getTime() - 1000 * 60 * 60 * 24 * 7) / 1000), Math.floor(new Date().getTime() / 1000)); 
-
-    } catch (e) { }
-
-    if (chartRef) {
-      if (chartManagerRef.current) {
-        chartManagerRef.current.destroy();
-      }
-      const chartManager = new ChartManager(
-        chartRef.current,
-        [
-          ...klineData?.map((x) => ({
-            close: parseFloat(x.close),
-            high: parseFloat(x.high),
-            low: parseFloat(x.low),
-            open: parseFloat(x.open),
-            timestamp: new Date(x.end), 
-          })),
-        ].sort((x, y) => (x.timestamp < y.timestamp ? -1 : 1)) || [],
-        {
-          background: "#0e0f14",
-          color: "white",
-        }
-      );
-      //@ts-ignore
-      chartManagerRef.current = chartManager;
-    }
-  };
-
   useEffect(() => {
+    const init = async () => {
+      let klineData: KLine[] = [];
+      try {
+        klineData = await getKlines(market, "1h", Math.floor((new Date().getTime() - 1000 * 60 * 60 * 24 * 7) / 1000), Math.floor(new Date().getTime() / 1000));
+
+      } catch (e) { }
+
+      if (chartRef) {
+        if (chartManagerRef.current) {
+          chartManagerRef.current.destroy();
+        }
+        const chartManager = new ChartManager(
+          chartRef.current,
+          [
+            ...klineData?.map((x) => ({
+              close: parseFloat(x.close),
+              high: parseFloat(x.high),
+              low: parseFloat(x.low),
+              open: parseFloat(x.open),
+              timestamp: new Date(x.end),
+            })),
+          ].sort((x, y) => (x.timestamp < y.timestamp ? -1 : 1)) || [],
+          {
+            background: "#0e0f14",
+            color: "white",
+          }
+        );
+        //@ts-ignore
+        chartManagerRef.current = chartManager;
+      }
+    };
+
     init();
   }, [market, chartRef]);
 
